af61e166632a83f90f5880337933e7a26cb70bc5
Kzulfazriawan <kzulfazriawan@gmail.com>
2022-12-06 16:52:01 +0700

    fixing the API backend

--- backend/models/variants.py
+++ backend/models/variants.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Column, Integer, ForeignKey
+
 from src.orm.fields import AutoIncrementId, Character, RichText, CreatedAt, UpdatedAt, RelationshipKey, Numeric
 from src.orm.model import Model, Queries
 
@@ -5,9 +7,9 @@ from src.orm.model import Model, Queries
 class Variants(Model, Queries):
     __tablename__ = "variants"
 
-    id = AutoIncrementId()
+    id = AutoIncrementId().field
     name = Character().field
-    product_id = RelationshipKey("products.id").field
+    product_id = Column(Integer, ForeignKey("images.id", ondelete="CASCADE"))
     description = RichText().field
     images = RichText(nullable=True).field
     size = Numeric().field
